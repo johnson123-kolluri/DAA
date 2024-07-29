606be8ca26d0ca15ffaa1f099691a6d2cc0d3752
johnson123-kolluri <johnson.kolluri@sru.edu.in>
2024-07-29 16:14:48 +0530

    Add files via upload

---
+++ insertionsort.py
@@ -0,0 +1,19 @@
+def insertion_sort(arr):
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        # Move elements of arr[0..i-1], that are greater than key,
+        # to one position ahead of their current position
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+    return arr
+
+# Example usage
+arr = [12, 11, 13, 5, 6]
+print("Original array:", arr)
+sorted_arr = insertion_sort(arr)
+print("Sorted array:", sorted_arr)
